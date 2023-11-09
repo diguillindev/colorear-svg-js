5d2f75465d76295a6513f09638da99181c9a940f
diguillindev <148013992+diguillindev@users.noreply.github.com>
2023-11-09 20:33:23 -0300

    Change UI Responsive

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # colorear-svg-js
-partial-index img-svg a elección del usuario, paleta de 12 colores
+partial-index img-svg a elección del usuario, paleta de 10 colores
--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-/* Función para ocultar el dropdown y mostrar el botón
-function hideDropdownAndShowButton() {
-  const dropdown = document.querySelector(".dropdown");
-  dropdown.style.display = "none";
-
-  // Mostrar el botón
-  const button = dropdown.querySelector("button");
-  button.style.display = "block";
+// Función para alternar el contenido del menú desplegable
+function toggleDropdown() {
+  const dropdown = document.getElementById("myDropdown");
+  dropdown.classList.toggle("show");
 }
 
-// Evento de clic para las imágenes
-const tinyImages = document.querySelectorAll("[data-tiny]");
+// Cierra el menú desplegable si el usuario hace clic fuera de él
+document.addEventListener("click", function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+});
 
-for (const img of tinyImages) {
-  img.addEventListener("click", function () {
-    // Lógica para seleccionar la imagen aquí
+// Attach event listener to the button
+const button = document.querySelector('.dropbtn');
+button.addEventListener('click', toggleDropdown);
 
-    // Ocultar el dropdown y mostrar el botón
-    hideDropdownAndShowButton();
-  });
-}
-*/
+
 
 // GAME OBJECT
 const game = {
@@ -33,8 +35,6 @@ const handler = {
   },
 };
 let proxyGame = new Proxy(game, handler);
-
-
 
 // INITIAL CURSOR
 document.body.style.cursor = `url('data:image/svg+xml;utf8,<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" height="15" width="15"><g id="Layer_1"><g id="Layer_1_1_"><g><path d="M6.563 2.297c2.297 0 4.172 1.875 4.172 4.172s-1.875 4.172 -4.172 4.172 -4.172 -1.875 -4.172 -4.172 1.875 -4.172 4.172 -4.172m0 -1.5C3.422 0.797 0.891 3.328 0.891 6.469S3.422 12.141 6.563 12.141s5.672 -2.531 5.672 -5.672S9.703 0.797 6.563 0.797z"/></g></g></g><g id="Layer_2"><path stroke="black" fill="${proxyGame.selectedColor}" cx="14" cy="13.6" r="8.9" d="M10.734 6.375A4.172 4.172 0 0 1 6.563 10.547A4.172 4.172 0 0 1 2.391 6.375A4.172 4.172 0 0 1 10.734 6.375z" stroke-width="0.46875"/></g></svg>'), auto`;
@@ -53,7 +53,7 @@ for (let i = 0; i < $controlColors.length; i++) {
   });
 }
 
-// SELECT ANY 
+// SELECT ANY IMG
 
 const $tinyImages = document.querySelector(".tiny-images");
 
@@ -113,3 +113,19 @@ function changeCursor() {
 
   document.body.style.cursor = $svgCursor;
 }
+
+// TOUR MAP
+const driver = window.driver.js.driver;
+
+const driverObj = driver({
+  showProgress: true,
+  steps: [
+    { element: '#my-button', popover: { title: 'Paso1', description: 'Elige entre nuestras obras'} },
+    { element: '#jsColors', popover: { title: 'Paso2', description: 'Toca un color y...¡A pintar!' } },
+  ],
+  nextBtnText: 'Siguiente', // Cambia el texto del botón "Next"
+  prevBtnText: 'Anterior', // Cambia el texto del botón "Previous"
+  doneBtnText: 'Cerrar', // Cambia el texto del botón "Done"
+});
+
+driverObj.drive();
